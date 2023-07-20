03b1b8c96371a703d9cff37add4b69901e6e8cf4
Purna Lakshitha <99383161+purnalakshitha99@users.noreply.github.com>
2023-07-20 10:48:58 +0530

    transitiion delay decrease

--- script.js
+++ script.js
@@ -118,7 +118,7 @@ document.addEventListener("DOMContentLoaded", function () {
     reset: true,
     distance: "80px",
     duration: 2000,
-    delay: 200,
+    delay: 50,
   });
 
   ScrollReveal().reveal(".home-content, .heading", { origin: "top" });
